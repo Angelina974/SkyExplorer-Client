9b87bfa35e876e21a0b5c8145324b3a5d9db7fa9
Angelina <julia.angelina.grossi@gmail.com>
2024-06-11 13:05:11 +0200

    update pdf model

--- views/invoices.js
+++ views/invoices.js
@@ -123,76 +123,79 @@ function getPdf() {
     const doc = new jsPDF();
 
     const img = new Image();
-    img.src = './resources/img/Facture.png';  // Utilisez le chemin relatif approprié
+    img.src = './resources/img/facture.png';  // Utilisez le chemin relatif approprié
 
     img.onload = function() {
         const imgProps = doc.getImageProperties(img);
         const pdfWidth = 210;
         const pdfHeight = 297;
         const startX = 20;
-        const columnX1 = 105;
+        const columnX1 = 120;
         const lineYadd = 7;
+        const lineYadd2 = 100
+        const lineYadd3 = lineYadd2 + 8
         let lineY;
 
 
         doc.addImage(img, 'PNG', 0, 0, pdfWidth, pdfHeight);
 
         //Gros titre
-        doc.setFont('Helvetica', "bold");          
-        doc.setFontSize(64);
-        doc.setTextColor("#24265a");
-        doc.text("Facture", startX, 32);
+        doc.setFont('Saira sans-serif', "bold");          
+        doc.setFontSize(30);
+        doc.setTextColor("#000055");
+        doc.text("Facture N° :", 120, 45);
 
 
         //description
-        doc.setFont('Helvetica', "bold");          
+        doc.setFont('Helvetica', "normal");          
         doc.setFontSize(14);
-        doc.setTextColor("#24265a");
+        doc.setTextColor("#000055");
 
         //${kiss.session.getUserName()}
         
-        doc.text("Facturé à : ", startX, 50);
-        doc.text("Référence :", columnX1, 50);
-        doc.text("Echéance le : ", columnX1, 58);
+        doc.text("Date :", columnX1, 60);
+        doc.text("Echéance : ", columnX1, 67);
+        doc.text("Client : ", columnX1, 74);
 
         doc.setFont('Helvetica', "normal");
-        doc.text(`${kiss.session.getUserName()}`, startX + 26, 50);
+        doc.text(`${kiss.session.getUserName()}`, columnX1 + 18, 74);
 
 
         //Tableur
         doc.setFont('Helvetica', "normal");          
+        doc.setFontSize(14);
+        doc.setTextColor("#000000");
+
+        doc.text("Référence", startX, lineYadd2);
+        doc.text("Date", startX + 40, lineYadd2);
+        doc.text("Client", startX + 80, lineYadd2);
+        doc.text("Type", startX + 120, lineYadd2);
+        doc.text("€HT/h", startX + 155, lineYadd2);
+
         doc.setFontSize(12);
-        doc.setTextColor("#24265a");
+        doc.setTextColor("#000055");
 
-        doc.text("Référence", startX, 85);
-        doc.text("Date", startX + 40, 85);
-        doc.text("Client", startX + 80, 85);
-        doc.text("Type", startX + 120, 85);
-        doc.text("€HT/h", startX + 155, 85);
-
-        doc.setFontSize(10);
-        doc.setTextColor("#0077c8");
-
-        lineY = 97;
         for (let i = 0; i < rowData.length; i++) {
-            doc.text(rowData[i]["columns"][0], startX, lineY); //Référence
-            doc.text(rowData[i]["columns"][1], startX + 40, lineY); //Date
-            doc.text(rowData[i]["columns"][3], startX + 80, lineY); //Client
-            doc.text(rowData[i]["columns"][9], startX + 120, lineY); //type du vol
-            doc.text(rowData[i]["columns"][4], startX + 155, lineY); //HT/h
+            doc.text(rowData[i]["columns"][0], startX, lineYadd3); //Référence
+            doc.text(rowData[i]["columns"][1], startX + 40, lineYadd3); //Date
+            doc.text(rowData[i]["columns"][3], startX + 80, lineYadd3); //Client
+            doc.text(rowData[i]["columns"][9], startX + 120, lineYadd3); //type du vol
+            doc.text(rowData[i]["columns"][4], startX + 155, lineYadd3); //HT/h
             
             lineY += lineYadd;
         }
 
-        doc.setDrawColor('#0077c8');
+        doc.setDrawColor('#000000');
         doc.setLineWidth(0.5);
         doc.setLineDash([3, 3], 0);
         doc.line(startX-4, 77, 192, 77)
         doc.line(startX-4, 89, 192, 89)
 
         //Reglement
-        doc.setFontSize(24);
-        doc.text("Règlement : ", startX, 220); //Effectué ou pas
+        doc.setFont('Helvetica', "normal");
+        doc.setFontSize(14);
+        doc.setTextColor("#000055");
+        doc.text("Modalités et conditions de paiement: ", startX, 220); //Effectué ou pas
 
         
         window.open(doc.output('bloburl'));
